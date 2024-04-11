755bdd4d373420f29b6e28280232adf310a043ee
Simone Albano <s.albano@sinapsys.it>
2024-04-11 16:38:56 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'db.json');
+const dbPath = path.join(__dirname, process.env.DB_PATH || 'db.json');
 
 let saved;
 
